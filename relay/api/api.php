9548884a46ed0e719a5311eecd875a2bf8f06d20
Odunayo Kusoro <okusoro@princeton.edu>
2016-03-31 08:55:10 -0400

    location services fix 4

--- relay/api/api.php
+++ relay/api/api.php
@@ -329,17 +329,13 @@ function send_location_mobile() {
 	$longitude = trim($body_params["longitude"]);
 
 	$response = send_location($latitude, $longitude);
-
-	echo "Not in response";
+	
 	if ($response["success"] == true){
-
-		echo "Response == true";
+		
 		$response_status_code = 201;
 		$return_array["success"] = $response["success"];
-		echo "Response == true After"
 	}
 
-	echo "Right before Return";
 	return_response($response_status_code, $return_array);
 
 }
